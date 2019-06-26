230c2d902645eaca7f8d37ed7a30b68d1b59ba15
Hadrien Kohl <hadrien.kohl@sonat.no>
2019-06-26 10:57:17 +0200

    Use mock server and local data in GsimDatasource

--- src/test/java/no/ssb/gsim/spark/GsimDatasourceTest.java
+++ src/test/java/no/ssb/gsim/spark/GsimDatasourceTest.java
@@ -1,24 +1,28 @@
 package no.ssb.gsim.spark;
 
-import org.apache.spark.SparkConf;
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okio.Buffer;
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static no.ssb.gsim.spark.GsimDatasource.*;
 
 public class GsimDatasourceTest {
 
     public static final String UNIT_DATASET_ID = "b9c10b86-5867-4270-b56e-ee7439fe381e";
-    private SparkConf config;
     private SQLContext sqlContext;
     private SparkContext sparkContext;
-    private File tempDirectory;
+    private MockWebServer server;
+    private MockResponse unitDatasetResponse;
 
     @After
     public void tearDown() throws Exception {
@@ -27,72 +31,62 @@ public class GsimDatasourceTest {
 
     @Before
     public void setUp() throws Exception {
-        this.config = new SparkConf()
-                .setAppName("testing provider")
-                .setMaster("local");
+        // Create temporary folder and copy test data into it.
+        File tempDirectory = Files.createTempDirectory("lds-gsim-spark").toFile();
+        InputStream parquetContent = this.getClass().getResourceAsStream("data/dataset.parquet");
+        Path parquetFile = tempDirectory.toPath().resolve("dataset.parquet");
+        Files.copy(parquetContent, parquetFile);
 
-        tempDirectory = Files.createTempDirectory("lds-gsim-spark").toFile();
+        // Setup a mock lds server.
+        this.server = new MockWebServer();
+        this.server.start();
+        HttpUrl baseUrl = server.url("/lds/");
 
-        config.set(CONFIG_LOCATION_PREFIX, tempDirectory.toString());
+        // Read the unit dataset json example.
+        InputStream in = this.getClass().getResourceAsStream("data/UnitDataSet_Person_1.json");
+        String json = new Buffer().readFrom(in).readByteString().utf8();
+        json = json.replaceAll("%DATA_PATH%", parquetFile.toString());
+        unitDatasetResponse = new MockResponse().setBody(json).setResponseCode(200);
 
-        config.set(CONFIG_LDS_OAUTH_TOKEN_URL,
-                "https://keycloak.staging.ssbmod.net/auth/realms/ssb/protocol/openid-connect/token");
+        SparkSession session = SparkSession.builder()
+                .appName(GsimDatasourceTest.class.getSimpleName())
+                .master("local")
+                .config("spark.ui.enabled", false)
+                .config(CONFIG_LDS_URL, baseUrl.toString())
+                .config(CONFIG_LDS_OAUTH_CLIENT_ID, "lds-c-postgres-gsim")
+                .config(CONFIG_LDS_OAUTH_USER_NAME, "api-user-3")
+                .config(CONFIG_LDS_OAUTH_PASSWORD, "890e9e58-b1b5-4705-a557-69c19c89dbcf")
+                .config(CONFIG_LOCATION_PREFIX, tempDirectory.toString())
+                .config(CONFIG_LDS_OAUTH_TOKEN_URL, "https://keycloak.staging.ssbmod.net/auth/realms/ssb/protocol/openid-connect/token")
+                .getOrCreate();
 
-        config.set(CONFIG_LDS_URL, "https://lds-c.staging.ssbmod.net/ns/");
-        config.set(CONFIG_LDS_OAUTH_CLIENT_ID, "lds-c-postgres-gsim");
-        config.set(CONFIG_LDS_OAUTH_USER_NAME, "api-user-3");
-        config.set(CONFIG_LDS_OAUTH_PASSWORD, "890e9e58-b1b5-4705-a557-69c19c89dbcf");
-
-        this.sparkContext = new SparkContext(config);
-        SparkSession session = SparkSession.builder().sparkContext(sparkContext).getOrCreate();
+        this.sparkContext = session.sparkContext();
         this.sqlContext = session.sqlContext();
+
     }
 
     @Test
-    public void testReadWithId() {
+    public void testReadWithId() throws IOException {
+        this.server.enqueue(unitDatasetResponse);
         Dataset<Row> dataset = sqlContext.read()
                 .format("no.ssb.gsim.spark")
                 .load("lds+gsim://" + UNIT_DATASET_ID);
-        dataset.show();
-    }
-
-    @Test
-    public void testReadWithPathAndId() {
-        Dataset<Row> dataset = sqlContext.read()
-                .format("no.ssb.gsim.spark")
-                .load("lds+gsim://UnitDataset/" + UNIT_DATASET_ID);
-        dataset.show();
-    }
-
-    @Test
-    public void testReadWithPrefixAndId() {
-        Dataset<Row> dataset = sqlContext.read()
-                .format("no.ssb.gsim.spark")
-                .load("lds+gsim://lds-postgres.staging.ssbmod.net/" + UNIT_DATASET_ID);
-        dataset.show();
-    }
-
-    @Test
-    public void testReadWithPrefixAndPathAndId() {
-        Dataset<Row> dataset = sqlContext.read()
-                .format("no.ssb.gsim.spark")
-                .load("lds+gsim://lds-postgres.staging.ssbmod.net/UnitDataset/" + UNIT_DATASET_ID);
         dataset.show();
     }
 
     @Test
     public void testWriteWithId() {
+        this.server.enqueue(unitDatasetResponse);
+        this.server.enqueue(unitDatasetResponse);
+        this.server.enqueue(new MockResponse().setResponseCode(201));
+        this.server.enqueue(unitDatasetResponse);
+
         Dataset<Row> dataset = sqlContext.read()
                 .format("no.ssb.gsim.spark")
                 .load("lds+gsim://" + UNIT_DATASET_ID);
+        dataset.printSchema();
         dataset.show();
 
-        Dataset<Row> dataset2 = sqlContext.read()
-                .format("no.ssb.gsim.spark")
-                .load("lds+gsim://" + UNIT_DATASET_ID);
-        Dataset<Row> distinct = dataset2.distinct();
-
-        //dataset.write().insertInto("test");
         dataset.write().format("no.ssb.gsim.spark").mode(SaveMode.Append).save("lds+gsim://" + UNIT_DATASET_ID);
 
     }
