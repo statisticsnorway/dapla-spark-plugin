eb7c2a93b6411ed80967d20b68d2d029d4eace83
RuneLind <rulind@gmail.com>
2019-10-13 16:54:03 +0200

    Add verification to testWriteAndCreateOfLdsObjects

--- src/test/java/no/ssb/gsim/spark/GsimDatasourceTest.java
+++ src/test/java/no/ssb/gsim/spark/GsimDatasourceTest.java
@@ -1,8 +1,14 @@
 package no.ssb.gsim.spark;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.ssb.lds.gsim.okhttp.InstanceVariable;
+import no.ssb.lds.gsim.okhttp.LogicalRecord;
+import no.ssb.lds.gsim.okhttp.UnitDataStructure;
+import no.ssb.lds.gsim.okhttp.UnitDataset;
 import okhttp3.HttpUrl;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import okio.Buffer;
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.*;
@@ -10,20 +16,24 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static no.ssb.gsim.spark.GsimDatasource.CONFIG_LDS_URL;
 import static no.ssb.gsim.spark.GsimDatasource.CONFIG_LOCATION_PREFIX;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GsimDatasourceTest {
 
-    public static final String UNIT_DATASET_ID = "b9c10b86-5867-4270-b56e-ee7439fe381e";
+    private static final String UNIT_DATASET_ID = "b9c10b86-5867-4270-b56e-ee7439fe381e";
     private SQLContext sqlContext;
     private SparkContext sparkContext;
     private MockWebServer server;
     private MockResponse unitDatasetResponse;
+    private File tempDirectory;
 
     @After
     public void tearDown() throws Exception {
@@ -33,7 +43,7 @@ public class GsimDatasourceTest {
     @Before
     public void setUp() throws Exception {
         // Create temporary folder and copy test data into it.
-        File tempDirectory = Files.createTempDirectory("lds-gsim-spark").toFile();
+        tempDirectory = Files.createTempDirectory("lds-gsim-spark").toFile();
         InputStream parquetContent = this.getClass().getResourceAsStream("data/dataset.parquet");
         Path parquetFile = tempDirectory.toPath().resolve("dataset.parquet");
         Files.copy(parquetContent, parquetFile);
@@ -89,7 +99,7 @@ public class GsimDatasourceTest {
     }
 
     @Test
-    public void testWriteAndCreateOfLdsObjects() throws InterruptedException {
+    public void testWriteAndCreateOfLdsObjects() throws InterruptedException, IOException {
         this.server.enqueue(unitDatasetResponse);
         this.server.enqueue(unitDatasetResponse);
         this.server.enqueue(new MockResponse().setResponseCode(201));
@@ -115,17 +125,99 @@ public class GsimDatasourceTest {
                 .option("create", "dataSetName")
                 .save();
 
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
-        System.out.println(server.takeRequest());
+        assertThat(getResponse().getMethod()).isEqualTo("GET");
+
+        checkUnitDataStructureResponse(dataStructure -> {
+            assertThat(dataStructure.getLogicalRecords().size()).isEqualTo(1);
+        });
+
+        checkUnitDataSetResponse(unitDataset -> {
+            assertThat(unitDataset.getDataSourcePath()).isEqualTo("/path");
+        });
+
+        checkLogicalRecordResponse(logicalRecord -> {
+            assertThat(logicalRecord.getShortName()).isEqualTo("spark_schema");
+        });
+
+        checkInstanceVariableResponse(instanceVariable -> {
+            assertThat(instanceVariable.getShortName()).isEqualTo("PERSON_ID");
+        });
+
+        checkInstanceVariableResponse(instanceVariable -> {
+            assertThat(instanceVariable.getShortName()).isEqualTo("INCOME");
+        });
+
+        checkInstanceVariableResponse(instanceVariable -> {
+            assertThat(instanceVariable.getShortName()).isEqualTo("GENDER");
+        });
+
+        checkInstanceVariableResponse(instanceVariable -> {
+            assertThat(instanceVariable.getShortName()).isEqualTo("MARITAL_STATUS");
+        });
+
+        checkInstanceVariableResponse(instanceVariable -> {
+            assertThat(instanceVariable.getShortName()).isEqualTo("MUNICIPALITY");
+        });
+
+        checkInstanceVariableResponse(instanceVariable -> {
+            assertThat(instanceVariable.getShortName()).isEqualTo("DATA_QUALITY");
+        });
+
+        checkLogicalRecordResponse(logicalRecord -> {
+            assertThat(logicalRecord.getShortName()).isEqualTo("spark_schema");
+        });
+
+        // Check that we have getDataSourcePath updated to correct path after parquet file is saved
+        checkUnitDataSetResponse(unitDataset -> {
+            assertThat(unitDataset.getDataSourcePath()).contains(tempDirectory.toString());
+        });
+    }
+
+    interface UnitDatasetAction {
+        void onRequest(UnitDataset unitDataset);
+    }
+
+    interface UnitDataStructureAction {
+        void onRequest(UnitDataStructure dataStructure);
+    }
+
+    interface LogicalRecordAction {
+        void onRequest(LogicalRecord logicalRecord);
+    }
+
+    interface InstanceVariableAction {
+        void onRequest(InstanceVariable instanceVariable);
+    }
+
+    private void checkUnitDataSetResponse(UnitDatasetAction unitDatasetAction) throws IOException, InterruptedException {
+        RecordedRequest recordedRequest = server.takeRequest();
+        ObjectMapper mapper = new ObjectMapper();
+        unitDatasetAction.onRequest(mapper.readValue(recordedRequest.getBody().readByteArray(), UnitDataset.class));
+    }
+
+    private void checkUnitDataStructureResponse(UnitDataStructureAction action) throws IOException, InterruptedException {
+        RecordedRequest recordedRequest = server.takeRequest();
+        ObjectMapper mapper = new ObjectMapper();
+        action.onRequest(mapper.readValue(recordedRequest.getBody().readByteArray(), UnitDataStructure.class));
+    }
+
+    private void checkLogicalRecordResponse(LogicalRecordAction action) throws IOException, InterruptedException {
+        RecordedRequest recordedRequest = server.takeRequest();
+        ObjectMapper mapper = new ObjectMapper();
+        action.onRequest(mapper.readValue(recordedRequest.getBody().readByteArray(), LogicalRecord.class));
+    }
+
+    private void checkInstanceVariableResponse(InstanceVariableAction action) throws IOException, InterruptedException {
+        RecordedRequest recordedRequest = server.takeRequest();
+        ObjectMapper mapper = new ObjectMapper();
+        action.onRequest(mapper.readValue(recordedRequest.getBody().readByteArray(), InstanceVariable.class));
+    }
+
+    private RecordedRequest getResponse() throws InterruptedException {
+        RecordedRequest recordedRequest = server.takeRequest();
+        String json = recordedRequest.getBody().readByteString().utf8();
+        System.out.println(json);
+
+        return recordedRequest;
     }
 }
