83b3f4233157c3cfc4b4612b9d9355eb31e0f966
RuneLind <rulind@gmail.com>
2020-01-14 10:56:44 +0100

    Load save dataset
    WIP

---
+++ src/main/java/no/ssb/dapla/service/SparkServiceClient.java
@@ -0,0 +1,12 @@
+package no.ssb.dapla.service;
+
+import com.squareup.okhttp.OkHttpClient;
+import no.ssb.dapla.catalog.protobuf.Dataset;
+
+public class SparkServiceClient {
+    OkHttpClient client = new OkHttpClient();
+
+    Dataset getDataset() {
+        return null;
+    }
+}
