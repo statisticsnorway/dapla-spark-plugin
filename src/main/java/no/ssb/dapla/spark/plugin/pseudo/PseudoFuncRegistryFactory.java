84b37a21c7ccd20b6ff23f46253829c07d6121cb
Kenneth Leine Schulstad <ks@udp.no>
2020-01-15 14:14:13 +0100

    Add pseudo function configs for fpe-text and fpe-fnr

--- src/main/java/no/ssb/dapla/spark/plugin/pseudo/PseudoFuncRegistryFactory.java
+++ src/main/java/no/ssb/dapla/spark/plugin/pseudo/PseudoFuncRegistryFactory.java
@@ -11,7 +11,8 @@ import no.ssb.dapla.dlp.pseudo.func.fpe.FpeFuncConfig;
 import java.util.List;
 
 public class PseudoFuncRegistryFactory {
-    private static final String KEY_ID = "01DWENC90WW9K41EN0QS2Q23X4"; // Used in DaplaKeyServiceDummyImpl
+    private static final String KEY_ID_1 = "01DWENC90WW9K41EN0QS2Q23X4"; // Used in DaplaKeyServiceDummyImpl
+    private static final String KEY_ID_2 = "01DWENCY4JP5VFWKTTS0KBA8QZ"; // Used in DaplaKeyServiceDummyImpl
 
     // This config should be fetched from elsewhere instead of being hardcoded
     static final List<PseudoFuncConfig> PSEUDO_CONFIG = ImmutableList.of(
@@ -19,13 +20,25 @@ public class PseudoFuncRegistryFactory {
         PseudoFuncConfig.Param.FUNC_NAME, "fpe-digits",
         PseudoFuncConfig.Param.FUNC_IMPL, FpeFunc.class.getName(),
         FpeFuncConfig.Param.ALPHABET, AlphabetType.DIGITS,
-        FpeFuncConfig.Param.KEY_ID, KEY_ID
+        FpeFuncConfig.Param.KEY_ID, KEY_ID_1
       )),
       new PseudoFuncConfig(ImmutableMap.of(
         PseudoFuncConfig.Param.FUNC_NAME, "fpe-alphanumeric",
         PseudoFuncConfig.Param.FUNC_IMPL, FpeFunc.class.getName(),
         FpeFuncConfig.Param.ALPHABET, AlphabetType.ALPHANUMERIC,
-        FpeFuncConfig.Param.KEY_ID, KEY_ID
+        FpeFuncConfig.Param.KEY_ID, KEY_ID_1
+      )),
+      new PseudoFuncConfig(ImmutableMap.of(
+      PseudoFuncConfig.Param.FUNC_NAME, "fpe-text",
+        PseudoFuncConfig.Param.FUNC_IMPL, FpeFunc.class.getName(),
+        FpeFuncConfig.Param.ALPHABET, AlphabetType.ALPHANUMERIC_WHITESPACE,
+        FpeFuncConfig.Param.KEY_ID, KEY_ID_1
+      )),
+      new PseudoFuncConfig(ImmutableMap.of(
+      PseudoFuncConfig.Param.FUNC_NAME, "fpe-fnr",
+        PseudoFuncConfig.Param.FUNC_IMPL, FpeFunc.class.getName(),
+        FpeFuncConfig.Param.ALPHABET, AlphabetType.DIGITS,
+        FpeFuncConfig.Param.KEY_ID, KEY_ID_2
       ))
     );
 
