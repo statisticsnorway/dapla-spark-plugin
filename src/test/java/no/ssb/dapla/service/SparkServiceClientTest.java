0c14f47de8ed793a917113e2a847cc7d869e908b
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-01-15 16:06:17 +0100

    Updated SparkServiceClientTest

--- src/test/java/no/ssb/dapla/service/SparkServiceClientTest.java
+++ src/test/java/no/ssb/dapla/service/SparkServiceClientTest.java
@@ -4,6 +4,7 @@ package no.ssb.dapla.service;
 import no.ssb.dapla.catalog.protobuf.Dataset;
 import org.apache.spark.SparkConf;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static no.ssb.dapla.service.SparkServiceClient.*;
@@ -17,10 +18,12 @@ public class SparkServiceClientTest {
         this.sparkConf.set(CONFIG_ROUTER_URL, "https://dapla-spark.staging-bip-app.ssb.no/");
         this.sparkConf.set(CONFIG_ROUTER_OAUTH_TOKEN_URL, "https://keycloak.staging-bip-app.ssb.no/auth/realms/ssb/protocol/openid-connect/token");
         this.sparkConf.set(CONFIG_ROUTER_OAUTH_GRANT_TYPE, "client_credential");
-        this.sparkConf.set(CONFIG_ROUTER_OAUTH_CLIENT_ID, "api-testing-rune-lind");
+        this.sparkConf.set(CONFIG_ROUTER_OAUTH_CLIENT_ID, System.getenv(CONFIG_ROUTER_OAUTH_CLIENT_ID));
+        this.sparkConf.set(CONFIG_ROUTER_OAUTH_CLIENT_SECRET, System.getenv(CONFIG_ROUTER_OAUTH_CLIENT_SECRET));
     }
 
     @Test
+    @Ignore
     public void test() {
         SparkServiceClient sparkServiceClient = new SparkServiceClient(this.sparkConf);
 
