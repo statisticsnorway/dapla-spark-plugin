47540f46975ded53f0aed247384aa12d63c4ce73
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-01-15 13:51:33 +0100

    Fixed spark conf

--- src/main/java/no/ssb/dapla/service/SparkServiceClient.java
+++ src/main/java/no/ssb/dapla/service/SparkServiceClient.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import no.ssb.dapla.catalog.protobuf.Dataset;
 import no.ssb.dapla.spark.plugin.OAuth2Interceptor;
 import no.ssb.dapla.utils.ProtobufJsonUtils;
-import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
--- src/test/java/no/ssb/dapla/service/SparkServiceClientTest.java
+++ src/test/java/no/ssb/dapla/service/SparkServiceClientTest.java
@@ -2,13 +2,8 @@ package no.ssb.dapla.service;
 
 
 import no.ssb.dapla.catalog.protobuf.Dataset;
-import no.ssb.dapla.gcs.token.delegation.BrokerDelegationTokenBinding;
-import no.ssb.dapla.spark.plugin.GsimDatasourceTest;
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
-import org.apache.spark.sql.SparkSession;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static no.ssb.dapla.service.SparkServiceClient.*;
@@ -16,24 +11,13 @@ import static no.ssb.dapla.service.SparkServiceClient.*;
 public class SparkServiceClientTest {
 
     private SparkConf sparkConf = new SparkConf();
+
     @Before
     public void setUp() {
-        // Mock user read by org.apache.hadoop.security.UserGroupInformation
-        System.setProperty("HADOOP_USER_NAME", "dapla-test");
-
-        // Read the unit dataset json example.
-        SparkSession session = SparkSession.builder()
-                .appName(GsimDatasourceTest.class.getSimpleName())
-                .master("local")
-                .config("spark.ui.enabled", false)
-                .config(CONFIG_ROUTER_URL , "https://dapla-spark.staging-bip-app.ssb.no/")
-                .config(CONFIG_ROUTER_OAUTH_TOKEN_URL , "https://keycloak.staging-bip-app.ssb.no/auth/realms/ssb/protocol/openid-connect/token")
-                .config(CONFIG_ROUTER_OAUTH_GRANT_TYPE , "client_credential")
-                .config(CONFIG_ROUTER_OAUTH_CLIENT_ID , "api-testing-rune-lind")
-                .config(CONFIG_ROUTER_OAUTH_USER_NAME , "api-testing-rune-lind")
-                .getOrCreate();
-
-        this.sparkConf.set(CONFIG_ROUTER_URL , "https://dapla-spark.staging-bip-app.ssb.no/");
+        this.sparkConf.set(CONFIG_ROUTER_URL, "https://dapla-spark.staging-bip-app.ssb.no/");
+        this.sparkConf.set(CONFIG_ROUTER_OAUTH_TOKEN_URL, "https://keycloak.staging-bip-app.ssb.no/auth/realms/ssb/protocol/openid-connect/token");
+        this.sparkConf.set(CONFIG_ROUTER_OAUTH_GRANT_TYPE, "client_credential");
+        this.sparkConf.set(CONFIG_ROUTER_OAUTH_CLIENT_ID, "api-testing-rune-lind");
     }
 
     @Test
