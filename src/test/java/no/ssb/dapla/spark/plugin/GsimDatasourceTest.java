e037defd59a9f1811887a91817cac29f3ce98206
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-01-19 21:58:34 +0100

    Added test for unathorized access, and cleaned up unused code.

--- src/test/java/no/ssb/dapla/spark/plugin/GsimDatasourceTest.java
+++ src/test/java/no/ssb/dapla/spark/plugin/GsimDatasourceTest.java
@@ -7,8 +7,6 @@ import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import no.ssb.dapla.catalog.protobuf.DatasetId;
 import no.ssb.dapla.gcs.oauth.GoogleCredentialsFactory;
 import no.ssb.dapla.gcs.token.delegation.BrokerDelegationTokenBinding;
@@ -27,18 +25,18 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -170,19 +168,31 @@ public class GsimDatasourceTest {
     public void testReadFromBucket() {
         final String location = "gs://" + blobId.getBucket() + "/" + blobId.getName();
 
-        no.ssb.dapla.catalog.protobuf.Dataset.Builder datasetBuilder = no.ssb.dapla.catalog.protobuf.Dataset.newBuilder()
+        no.ssb.dapla.catalog.protobuf.Dataset datasetMock = no.ssb.dapla.catalog.protobuf.Dataset.newBuilder()
                 .setId(DatasetId.newBuilder().setId("mockId").addName("dapla.namespace").build())
                 .setValuation(no.ssb.dapla.catalog.protobuf.Dataset.Valuation.valueOf("SENSITIVE"))
                 .setState(no.ssb.dapla.catalog.protobuf.Dataset.DatasetState.valueOf("INPUT"))
-                .addLocations(location);
+                .addLocations(location).build();
 
-        server.enqueue(new MockResponse().setBody(ProtobufJsonUtils.toString(datasetBuilder.build())).setResponseCode(200));
+        server.enqueue(new MockResponse().setBody(ProtobufJsonUtils.toString(datasetMock)).setResponseCode(200));
         Dataset<Row> dataset = sqlContext.read()
                 .format("gsim")
                 .load("dapla.namespace");
 
         assertThat(dataset).isNotNull();
         assertThat(dataset.isEmpty()).isFalse();
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testUnauthorizedReadShouldFail() {
+        server.enqueue(new MockResponse().setResponseCode(403));
+        thrown.expectMessage("Din bruker dapla-test har ikke tilgang til dapla.namespace");
+        sqlContext.read()
+                .format("gsim")
+                .load("dapla.namespace");
     }
 
     @Test
@@ -203,86 +213,4 @@ public class GsimDatasourceTest {
 
     }
 
-    @Test
-    @Ignore("Dapla plugin server is not yet implemented")
-    public void testReadWithIdAndCallServer() {
-        try (SparkPluginTestServer sparkPluginTestServer = new SparkPluginTestServer(9123)) {
-            sparkPluginTestServer.start();
-
-            Dataset<Row> dataset = sqlContext.read()
-                    .option("uri_to_dapla_plugin_server", "localhost:9123")
-                    .load(parquetFile.toString());
-
-            assertThat(dataset).isNotNull();
-            assertThat(dataset.isEmpty()).isFalse();
-
-            List<String> responses = sparkPluginTestServer.getResponses();
-            assertThat(responses.size()).isEqualTo(1);
-            assertThat(responses.get(0)).startsWith("Hello from dapla-spark-plugin!");
-        } catch (InterruptedException | IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    static class SparkPluginTestServer implements AutoCloseable {
-        private final Server server;
-        private AtomicBoolean isRunning = new AtomicBoolean(false);
-
-        public List<String> getResponses() {
-            return responses;
-        }
-
-        private ArrayList<String> responses = new ArrayList<>();
-
-        public SparkPluginTestServer(int port) {
-            server = ServerBuilder.forPort(port)
-                    //.addService(new SparkPluginService())
-                    .build();
-        }
-
-        public void start() throws IOException {
-            server.start();
-            isRunning.set(true);
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                @Override
-                public void run() {
-                    if (isRunning.get()) {
-                        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                        System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                        SparkPluginTestServer.this.stop();
-                        System.err.println("*** server shut down");
-                    } else {
-                        System.out.println("Is already shutting down");
-                    }
-                }
-            });
-        }
-
-        @Override
-        public void close() throws InterruptedException {
-            isRunning.set(false);
-            System.out.println("SparkPluginTestServer - close");
-            stop();
-            blockUntilShutdown();
-        }
-
-        /**
-         * Stop serving requests and shutdown resources.
-         */
-        public void stop() {
-            if (server != null) {
-                server.shutdown();
-            }
-        }
-
-        /**
-         * Await termination on the main thread since the grpc library uses daemon threads.
-         */
-        private void blockUntilShutdown() throws InterruptedException {
-            if (server != null) {
-                server.awaitTermination();
-            }
-        }
-
-    }
 }
