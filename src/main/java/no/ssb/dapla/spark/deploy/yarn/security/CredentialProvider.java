c597da8b8cf082b34985af28edca93755d182f29
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-01-24 23:25:44 +0100

    Testing experimental CredentialProvider

---
+++ src/main/java/no/ssb/dapla/spark/deploy/yarn/security/CredentialProvider.java
@@ -0,0 +1,37 @@
+package no.ssb.dapla.spark.deploy.yarn.security;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.Credentials;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.spark.SparkConf;
+import org.apache.spark.deploy.security.HadoopDelegationTokenProvider;
+import org.apache.spark.deploy.yarn.security.ServiceCredentialProvider;
+import scala.Option;
+
+public class CredentialProvider implements ServiceCredentialProvider, HadoopDelegationTokenProvider {
+
+    @Override
+    public String serviceName() {
+        throw new RuntimeException("test serviceName");
+    }
+
+    @Override
+    public boolean credentialsRequired(Configuration configuration) {
+        return true;
+    }
+
+    @Override
+    public Option<Object> obtainCredentials(Configuration configuration, SparkConf sparkConf, Credentials credentials) {
+        throw new RuntimeException("test obtainCredentials");
+    }
+
+    @Override
+    public boolean delegationTokensRequired(SparkConf sparkConf, Configuration configuration) {
+        throw new RuntimeException("test serviceName");
+    }
+
+    @Override
+    public Option<Object> obtainDelegationTokens(Configuration configuration, SparkConf sparkConf, Credentials credentials) {
+        throw new RuntimeException("test obtainDelegationTokens");
+    }
+}
