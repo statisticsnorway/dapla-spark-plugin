761bac773ef809242a9acd41f42c67d10a3adb17
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-03-06 10:27:36 +0100

    Add environment variables for master and deployMode

--- docker/zeppelin/env.sh
+++ docker/zeppelin/env.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 echo "Replacing environment variables"
-envsubst \
-	' \
-	 $DAPLA_SPARK_GCS_STORAGE \
-	 $DAPLA_SPARK_DATA_ACCESS_URL \
-	 $DAPLA_SPARK_METADATA_PUBLISHER_URL \
-	 $DAPLA_SPARK_METADATA_PUBLISHER_PROJECT_ID \
-	 $DAPLA_SPARK_METADATA_PUBLISHER_TOPIC_NAME \
-	 $DAPLA_SPARK_OAUTH_TOKEN_URL \
-	 $DAPLA_SPARK_OAUTH_CREDENTIALS_FILE \
-	 $DAPLA_SPARK_OAUTH_CLIENT_ID \
-	 $DAPLA_SPARK_OAUTH_CLIENT_SECRET \
-	 ' < /zeppelin/conf/interpreter-template.json > /zeppelin/conf/interpreter.json
+envsubst < /zeppelin/conf/interpreter-template.json > /zeppelin/conf/interpreter.json
 
 /zeppelin/bin/zeppelin.sh
