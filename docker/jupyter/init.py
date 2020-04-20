fd37bc2d2949290e1d73b770e8fd93ea91d5dc76
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-04-20 12:17:07 +0200

    Created a kernel for running local pyspark.

--- docker/jupyter/init.py
+++
@@ -1,7 +0,0 @@
-import os
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName('jupyter') \
-    .config("spark.ssb.access", os.environ["SSB_ACCESS"]) \
-    .config("spark.ssb.refresh", os.environ["SSB_REFRESH"]) \
-    .getOrCreate()
---
+++ docker/jupyter/kernels/pyspark-local/init.py
@@ -0,0 +1,11 @@
+import os
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName(os.environ["JUPYTERHUB_CLIENT_ID"]) \
+    .config("spark.ssb.access", os.environ["SSB_ACCESS"]) \
+    .config("spark.ssb.refresh", os.environ["SSB_REFRESH"]) \
+    .config("spark.ssb.dapla.oauth.tokenUrl", os.environ["OAUTH2_TOKEN_URL"]) \
+    .config("spark.ssb.dapla.metadata.publisher.url", os.environ["METADATA_PUBLISHER_URL"]) \
+    .config("spark.ssb.dapla.data.access.url", os.environ["DATA_ACCESS_URL"]) \
+    .config("spark.ssb.dapla.catalog.url", os.environ["CATALOG_URL"]) \
+    .getOrCreate()
