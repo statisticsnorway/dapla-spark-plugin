bd340e40d39ef65a6965c1771848eba8a7472c78
Rune Lind <ssb.rlind@gmail.com>
2020-07-02 10:29:21 +0200

    Add toLineageTemplate to SparkSchemaConverter (#50)
    For use from dapla-gcp-jupyter sparkextension.py

--- src/main/java/no/ssb/dapla/spark/plugin/SparkSchemaConverter.java
+++ src/main/java/no/ssb/dapla/spark/plugin/SparkSchemaConverter.java
@@ -1,16 +1,38 @@
 package no.ssb.dapla.spark.plugin;
+
+import no.ssb.dapla.dataset.doc.builder.LineageBuilder;
+import no.ssb.dapla.dataset.doc.model.lineage.SchemaWithPath;
 import no.ssb.dapla.dataset.doc.template.SchemaToTemplate;
 import org.apache.avro.Schema;
 import org.apache.spark.sql.avro.SchemaConverters;
 import org.apache.spark.sql.types.StructType;
 
+import java.util.List;
+import java.util.Map;
+
 public class SparkSchemaConverter {
 
     public static String toSchemaTemplate(StructType sparkSchema, String simple) {
         Schema schema = SchemaConverters.toAvroType(sparkSchema, false, "spark_schema", "");
-        SchemaToTemplate generator = new SchemaToTemplate(schema, null);
-        return generator.withDoSimpleFiltering(Boolean.valueOf(simple)).generateSimpleTemplateAsJsonString();
+        SchemaToTemplate generator = new SchemaToTemplate(schema);
+        return generator.withDoSimpleFiltering(Boolean.parseBoolean(simple)).generateSimpleTemplateAsJsonString();
     }
+
+    public static String toLineageTemplate(StructType sparkOutputSchema, List<Map<String, StructType>> mapList) {
+        Schema outputSchema = SchemaConverters.toAvroType(sparkOutputSchema, false, "spark_schema", "");
+        LineageBuilder.SchemaToLineageBuilder lineageBuilder =
+                LineageBuilder.createSchemaToLineageBuilder()
+                        .outputSchema(outputSchema);
+
+        for (Map<String, StructType> item : mapList) {
+            for (Map.Entry<String, StructType> entry : item.entrySet()) {
+                Schema inputSchema = SchemaConverters.toAvroType(entry.getValue(), false, "spark_schema", "");
+                lineageBuilder.addInput(new SchemaWithPath(inputSchema, entry.getKey(), 123));
+            }
+        }
+        return lineageBuilder.build().generateTemplateAsJsonString();
+    }
+
 
     public static Schema toAvroSchema(StructType sparkSchema, String recordName, String recordNamespace) {
         return SchemaConverters.toAvroType(sparkSchema, false, recordName, recordNamespace);
